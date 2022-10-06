b6985f206779e9dc37dd6676a75becf075dee466
Daniel Avila <roicesdaniel@gmail.com>
2022-10-06 18:51:46 -0400

    poniendo esa cosa en basename en app.jsx funciona, ahora faltan las rutas xd

--- src/utils/ensure-basename.js
+++
@@ -1,9 +0,0 @@
-import { config } from '../config';
-
-if (!window.location.pathname.includes(config.ROUTER_BASE_URL)) {
-  window.history.replaceState(
-    '',
-    '',
-    config.ROUTER_BASE_URL + window.location.pathname
-  );
-}
